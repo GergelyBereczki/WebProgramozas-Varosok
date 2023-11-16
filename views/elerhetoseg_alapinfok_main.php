badd34cc68785be32b5fb2e8342d23d00a1656b1
Bagó Bence József <106177569+Sh4dyGh0st@users.noreply.github.com>
2023-11-16 21:34:54 +0100

    Add files via upload

---
+++ views/elerhetoseg_alapinfok_main.php
@@ -0,0 +1,12 @@
+<h2>
+    <br>Alapinfók:<br>
+    <br>Neumann János Egyetem GAMF Műszaki és Informatikai Kar
+<br>
+<br>Cím: Kecskemét, Izsáki út 10, 6000<br>
+
+    <br>Telefon: 0676516300<br>
+
+    <br><a href="http://gamf.uni-neumann.hu/" target="_blank">http://gamf.uni-neumann.hu/</a><br>
+
+    
+</h2>
---
+++ views/elerhetoseg_kiegeszitesek_main.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <script type="text/javascript" src = "jquery.min.js"></script>
+  <script type="text/javascript" src = "../includes/varosok.js"></script>
+  <title>Városok</title>
+  <style>
+    #informaciosdiv {
+      width: 400px;
+    }
+    #intezmenyinfo {
+      float: right;
+      border: 1px solid black;
+      width: 190px;
+      height: 100px;
+    }
+    .cimke{
+      display: inline-block;
+      width: 60px;
+      text-align: right;
+    }
+    span {
+      margin: 3px 5px;
+    }
+    label {
+      display: inline-block;
+      width: 70px;
+      text-align: right;
+    }
+    select {
+      width: 115px;
+    }
+  </style>
+  </head>
+  <body>
+    <h1>Város adatai:</h1>
+    <div id = 'informaciosdiv'>
+      <div id = 'intezmenyinfo'>
+        <span class="cimke">Év:</span><span id="ev" class="adat"></span><br>
+        <span class="cimke">Nő:</span><span id="no" class="adat"></span><br>
+        <span class="cimke">Összesen:</span><span id="osszesen" class="adat"></span><br>
+       
+      </div>
+      <label for='mecimke'>Megye:</label>
+      <select id = 'meselect'></select>
+      <br><br>
+      <label for = 'vkcimke'>Város:</label>
+      <select id = 'vkselect'></select>
+      <br><br>
+      <label for = 'lszcimke'>Lélekszám:</label>
+      <select id = 'lszselect'></select>
+    </div>
+  </body>
+</html>
